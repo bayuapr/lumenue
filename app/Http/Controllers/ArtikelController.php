2270947cb78a549794465ca74af8147b1a7f12c4
bayuapr <bayuapriansyah7@gmail.com>
2021-02-01 07:14:16 +0700

    Belajar Lumen

---
+++ app/Http/Controllers/ArtikelController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ArtikelController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        echo "Coba ";
+    }
+
+    public function get(){
+        echo "Get Kaktus";
+    }
+    
+}
